40ae0d5f76a88f22072337517efafbe21fdf206d
syifaaf21 <syifafauziah0921@gmail.com>
2025-09-22 11:14:42 +0700

    Initial commit

---
+++ database/migrations/2025_09_22_040344_create_document_mapping_table.php
@@ -0,0 +1,38 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::create('document_mappings', function (Blueprint $table) {
+            $table->id();
+            $table->foreignId('document_id')->constrained('documents')->onDelete('cascade');
+            $table->foreignId('part_number_id')->constrained('part_numbers')->onDelete('cascade');
+            $table->foreignId('status_id')->constrained('statuses')->onDelete('cascade');
+            $table->string('document_number');
+            $table->enum('type', ['control','review']);
+            $table->string('version');
+            $table->string('file_path');
+            $table->text('notes');
+            $table->date('obsolete_date');
+            $table->date('reminder_date');
+            $table->date('deadline');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::dropIfExists('document_mapping');
+    }
+};
