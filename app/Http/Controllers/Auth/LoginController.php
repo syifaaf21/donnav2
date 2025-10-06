4551a413e60204f25e9b41350e8c15804de79e34
s-lestari <sanya.lestari@president.ac.id>
2025-10-06 07:22:48 +0700

    improve login controller redirect to dashboard

--- app/Http/Controllers/Auth/LoginController.php
+++ app/Http/Controllers/Auth/LoginController.php
@@ -22,7 +22,7 @@ class LoginController extends Controller
 
         if (Auth::attempt($credentials, $request->filled('remember'))) {
             $request->session()->regenerate();
-            return redirect()->route('users.index'); // Ganti sesuai tujuan
+            return redirect()->route('dashboard'); // Ganti sesuai tujuan
         }
 
         return back()->withErrors([
