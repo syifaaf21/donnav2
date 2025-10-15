73291713dfd140ad878d95e759ce8bed9f761d93
s-lestari <sanya.lestari@president.ac.id>
2025-10-15 07:29:22 +0700

    merge main into sanya

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -5,6 +5,7 @@
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta name="csrf-token" content="{{ csrf_token() }}">
     <title>@yield('title', 'Dashboard')</title>
 
     {{-- ✅ Include Tailwind via Vite --}}
@@ -25,9 +26,9 @@
     {{-- ✅ Google Fonts --}}
     <link href="https://fonts.googleapis.com/css2?family=Inter:wght@400;600&display=swap" rel="stylesheet">
 
-    <script src="https://cdn.tailwindcss.com"></script>
+    {{-- <script src="https://cdn.tailwindcss.com"></script> --}}
     <link href="https://cdn.jsdelivr.net/npm/select2@4.1.0-rc.0/dist/css/select2.min.css" rel="stylesheet" />
-    <meta name="csrf-token" content="{{ csrf_token() }}">
+
 
 
     @stack('styles')
