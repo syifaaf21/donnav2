6633df8a2e845dd69eb7eb5048220592be4508c1
s-lestari <sanya.lestari@president.ac.id>
2025-12-02 13:09:28 +0700

    minimize footer text

--- resources/views/layouts/partials/footer.blade.php
+++ resources/views/layouts/partials/footer.blade.php
@@ -1,16 +1,12 @@
-<footer class="footer py-3">
-    <div class="container-fluid">
-        <div class="row align-items-center">
-            <div class="col-md-6">
-                <span class="text-color">
-                    &copy; {{ date('Y') }} <strong>Madonna</strong> - Management of Document and Operation Network Aisin
-                </span>
-            </div>
-            <div class="col-md-6 text-end">
-                <span class="text-color">
-                    Powered by <strong>AISIN Indonesia Automotive</strong>
-                </span>
-            </div>
+<footer class="py-4 mt-8">
+    <div class="max-w-8xl mx-auto px-4 flex flex-col md:flex-row items-center justify-between">
+        <div class="text-gray-600 text-sm md:text-base mb-2 md:mb-0 text-center md:text-left">
+            &copy; {{ date('Y') }} <strong class="font-semibold">Madonna</strong> - Management of Document and Operation Network Aisin
+        </div>
+        <div class="text-gray-600 text-sm md:text-base text-center md:text-right flex items-center justify-center space-x-2">
+            <span>Powered by</span>
+            <strong class="font-semibold">AISIN Indonesia Automotive</strong>
         </div>
     </div>
 </footer>
+
