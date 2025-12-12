caeee98a2ac864a153793e0dcaa0362005e2b4fb
syifaaf21 <syifafauziah0921@gmail.com>
2025-12-12 07:56:43 +0700

    Update whatsapp reminder

--- app/Services/WhatsAppService.php
+++ app/Services/WhatsAppService.php
@@ -30,11 +30,11 @@ class WhatsAppService
         $groupId = $groupId ?: $this->defaultGroupId;
 
         // Simulasi jika environment local
-        if (config('app.env') === 'local') {
-            Log::info("[LOCAL MODE] WhatsApp simulated send to group {$groupId}");
-            Log::info("Message:\n{$message}");
-            return true;
-        }
+        // if (config('app.env') === 'local') {
+        //     Log::info("[LOCAL MODE] WhatsApp simulated send to group {$groupId}");
+        //     Log::info("Message:\n{$message}");
+        //     return true;
+        // }
 
         // Sesuaikan endpoint dan parameter sesuai dokumentasi FastWA.
         // Misalnya endpoint: /send_group atau /send_text_group
