e99a032f498ac50be0352ab06ea1fc8b77bfbf3f
syifaaf21 <syifafauziah0921@gmail.com>
2026-01-23 15:26:54 +0700

    Update notif doc control

--- app/Console/Commands/SendDocumentControlReminder.php
+++ app/Console/Commands/SendDocumentControlReminder.php
@@ -15,7 +15,7 @@ class SendDocumentControlReminder extends Command
     public function handle(WhatsAppService $wa)
     {
         $today = Carbon::now();
-        if (!$today->isFriday()) {
+        if (!$today->isMonday()) {
             $this->info("Not Monday, skipping WhatsApp reminder.");
             return;
         }
